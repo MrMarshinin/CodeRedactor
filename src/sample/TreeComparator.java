619b9342c24d7406370b64465fd544468be0f5e4
MrMarshinin <mr.marshinin@mail.ru>
2020-05-04 19:46:40 +0300

    Final version.

--- src/sample/TreeComparator.java
+++ src/sample/TreeComparator.java
@@ -4,24 +4,28 @@ public class TreeComparator {
     private Tree previousTree;
 
     public void update(String string) throws IncorrectInputException {
-        Parser parser = new Parser(string);
+        try {
+            Parser parser = new Parser(string);
 
-        if (previousTree == null) {
-            previousTree = new Tree(parser.getStatementList());
-            if (previousTree.containsNotEmptyIfStatement()) {
-                previousTree.notifyAllIfStatements(true);
+            if (previousTree == null) {
+                previousTree = new Tree(parser.getStatementList());
+                if (previousTree.containsNotEmptyIfStatement()) {
+                    previousTree.notifyAllIfStatements(true);
+                    System.out.println("If statement was added");
+                }
+                return;
+            }
+
+            Tree currentTree = new Tree(parser.getStatementList());
+
+            if (previousTree.wasIfStatementAdded(currentTree)) {
                 System.out.println("If statement was added");
             }
-            return;
+
+            currentTree.notifyNotEmptyIfStatements(true);
+            previousTree = currentTree;
+        } catch (Exception e){
+            throw new IncorrectInputException();
         }
-
-        Tree currentTree = new Tree(parser.getStatementList());
-
-        if (previousTree.wasIfStatementAdded(currentTree)) {
-            System.out.println("If statement was added");
-        }
-
-        currentTree.notifyNotEmptyIfStatements(true);
-        previousTree = currentTree;
     }
 }
